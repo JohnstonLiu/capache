760563f648fc0b2a26e12914918bb442e289ac1b
Johnston Liu <johnstonliu2004@gmail.com>
2025-12-28 12:50:32 -0800

    Initial Commit

---
+++ cache/cacheApp.swift
@@ -0,0 +1,17 @@
+//
+//  cacheApp.swift
+//  cache
+//
+//  Created by Johnston Liu on 2025-12-28.
+//
+
+import SwiftUI
+
+@main
+struct cacheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
